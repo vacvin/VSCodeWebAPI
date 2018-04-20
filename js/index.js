5fdbd75996d9d1cd17ba4c22edaed97fddea8733
vince.su <vacvin@gmail.com>
2018-04-20 11:38:38 +0800

    use component to replace user list region.

--- js/index.js
+++ js/index.js
@@ -9,6 +9,12 @@ window.onload = function () {
         },
         methods: {
             QueryUser: function (event) {
+                if(event.target.value == '')
+                {
+                    this.QueryUserAll();
+                    return;
+                }
+
                 $.ajax({
                     context: this,
                     url: 'api/user/' + event.target.value,
@@ -24,9 +30,10 @@ window.onload = function () {
                     url: 'api/user',
                     success: function(res){
                         this.users = res;
+                        this.$emit('testevent','123');
                     }
                 });
-            },
+            },            
             addUser: function () {
                 var data = {
                     Name: this.name,
@@ -56,5 +63,36 @@ window.onload = function () {
         }
     })
 
+    Vue.component('userlist-header', {
+        template: '<div>' + 
+                  '<div class="header cell w20">ID</div>' + 
+                  '<div class="header cell w80">Name</div>' + 
+                  '<div class="header cell w160">Birthday</div>' +
+                  '<div class="header cell w240">CreateTime</div>' +
+                  '<div class="header cell w80">Delete</div>' +
+                  '</div>'
+    });
+
+    Vue.component('userlist', {
+        props: ['user'],
+        template: '<div>' + 
+                  '<div class="cell w20">{{ user.id }}</div>' + 
+                  '<div class="cell w80">{{ user.name }}</div>' + 
+                  '<div class="cell w160">{{ user.birthday }}</div>' +
+                  '<div class="cell w240">{{ user.createTime }}</div>' +
+                  '<div class="cell w80"><button v-on:click="deleteUser(user.id)">Delete</button></div>' +
+                  '</div>',
+        methods: {
+            deleteUser: function (id) {
+                //this.$emit('deleteUser', id);
+                this.$parent.deleteUser(id);
+            }
+        },
+    });
+    
     this.app.QueryUserAll();
+
+    this.app.$on('testevent', function(value){
+        //Do something
+    }, this);
 }
